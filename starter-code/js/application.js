9d3b52d93f0150784be46ae05898e3a07a125890
Santiago Aldanondo <santiagoaldanondo@gmail.com>
2017-05-08 01:32:33 +0200

    Working before restart. Check red input on error, CSS, restart time left and fourth iteration.

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -2,7 +2,75 @@
 
 //Initialize ion library
 
+var game = {};
+$("#start-button").on("click", function() {
+	$("#game-options").hide();
+	$("#game-board").show();
 
-window.onload = function(){
+	// Create an array with the operators that have been checked
+	var activeOperators = [];
+	if (document.getElementById("check-add").checked) {
+		activeOperators.push("+");
+	}
+	if (document.getElementById("check-substract").checked) {
+		activeOperators.push("-");
+	}
+	if (document.getElementById("check-multiply").checked) {
+		activeOperators.push("*");
+	}
+	if (document.getElementById("check-divide").checked) {
+		activeOperators.push("/");
+	}
 
-};
+	// Assign a new TenSecondsMathGame to the variable game. The variable game has been
+	// created outside of the function in order to be accesible to other functions
+	game = new TenSecondsMathGame(activeOperators, document.querySelector("#range-value").value);
+	printQuestion();
+});
+
+function printQuestion() { // Prints the new question in the HTML
+	document.querySelector("#question-value").innerHTML = game.newQuestion();
+}
+
+function rangeUpdate(inputValue) { // Updates the value printed for the range when moving the slider
+	document.querySelector("#range-value").value = inputValue;
+}
+
+function checkAnswer(inputValue) { // Checks if the written answer is ok
+
+	if (game.isCorrectAnswer(parseInt(document.querySelector("#answer-input").value))) {
+
+		// Empty the input box
+		document.querySelector("#answer-input").value = "";
+
+		// Generate another question
+		printQuestion();
+	}
+}
+
+function printTime() {
+	if (timeLeft <= 0) {
+		$("#clock").hide();
+		$("#game-over").show();
+		$("#restart-button").show();
+		document.querySelector("#answer-input").disabled = true;
+		return;
+	} else {
+		setTimeout(function() {
+			document.querySelector("#time-left").innerHTML = timeLeft;
+			printTime();
+		}, 1000);
+	}
+}
+
+$("#restart-button").on("click", function() {
+	$("#clock").show();
+	$("#game-over").hide();
+	$("#game-board").hide();
+	$("#game-options").show();
+	$("#restart-button").hide();
+	document.querySelector("#answer-input").disabled = false;
+	timeLeft = 10;
+});
+
+printTime();
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,87 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var timeLeft = 10;
 
+var TenSecondsMathGame = function(operators, numberLimit) {
+	this.operators = operators;
+	this.numberLimit = numberLimit;
+	this.answer = 0;
+	this.correctAnswers = 0;
+
+  // Returns a random integer between [1..numberLimit]
+	this.generateRandom = function() {
+		return Math.floor(Math.random() * (this.numberLimit - 1) + 1);
+	};
+
+  // Returns an object with {question, answer}
+	this.newQuestion = function() {
+		var operator = this.operators[Math.floor(Math.random() * this.operators.length)];
+
+		if (operator === "/") { // Rules for division
+
+      // First calculate the result and the divisor, that must be within the limits
+			this.answer = this.generateRandom();
+			var secondOperand = this.generateRandom();
+
+      // Calculate the dividend
+			var firstOperand = secondOperand * this.answer;
+
+		} else { // Rules for addition, substraction and multiplication
+			var secondOperand = this.generateRandom();
+			var firstOperand = this.generateRandom();
+
+      // Check rules about operands when the operator is not a division
+			while (Math.abs(firstOperand - secondOperand) <= 3 ||
+      firstOperand === secondOperand) {
+				secondOperand = this.generateRandom();
+				firstOperand = this.generateRandom();
+			}
+
+      // Change operands when substraction gives a negative result
+			if (operator === "-" && firstOperand - secondOperand < 0) {
+				var aux = firstOperand;
+				firstOperand = secondOperand;
+				secondOperand = aux;
+			}
+		}
+
+		var question = firstOperand + " " + operator + " " + secondOperand;
+		this.answer = eval(question);
+		return question;
+	};
+
+  // Checks a user answer
+	this.isCorrectAnswer = function(result) {
+		var isOk = result === this.answer;
+
+		// Checks if it is correct
+		if (isOk) {
+
+			// Add 1 to the number of correctAnswers
+			this.correctAnswers++;
+
+			// Add 1 second to the timeLeft
+			timeLeft += 1;
+
+			// For the first correct answer you start the countDown
+			if (this.correctAnswers === 1) {
+				_startTimer();
+			}
+		}
+		return isOk;
+	};
+	function _startTimer() {
+		if (_checkTimer() > 0) {
+			setTimeout(function() {
+				timeLeft -= 1;
+				_startTimer();
+			}, 1000);
+		} else {
+			return true;
+		}
+	};
+
+	function _checkTimer() {
+		return timeLeft;
+	}
 };
-
-// Returns a random integer between [1..numberLimit]
-
-
-// Returns an object with {question, answer}
-
-
-// Checks a user answer
